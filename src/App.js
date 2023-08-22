b27ca3a705f33db6d0ef39dad9645d32b16e5a24
Umesh <umeshmane575@gmail.com>
2023-08-22 10:35:11 +0530

    i have added ant-design css nav bar bcreation is it showing beautiful navigation bar with Home username login and Register working fine

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Switch , Route} from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home"; 
+import Header from "./components/Header";
+
 function App() {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/login" component={Login}/>
       <Route exact  path="/register" component={Register}/>
-
     </Switch>
+    </>
   );
 }
 
