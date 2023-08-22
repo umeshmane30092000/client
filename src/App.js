b22f885481fd7d0924e830181e4ed7dbe4b34b0a
Umesh <umeshmane575@gmail.com>
2023-08-22 09:39:54 +0530

    routes setting is done working fine if we click on  the pages like login register home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from "react";
+import {Switch , Route} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home"; 
 function App() {
   return (
-    <div >
-    <p>React App</p>
-      
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact  path="/register" component={Register}/>
+
+    </Switch>
   );
 }
 
