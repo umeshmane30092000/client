18491c43a588f4c8c1fc34ce266b804a1fb563e9
Umesh <umeshmane575@gmail.com>
2023-08-22 12:41:13 +0530

    firebase is set up goole and imported required code in firebase.js for user authentication

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPgIh2eM2LnYPmb8qsS1G3vbSLFR2tDBg",
+    authDomain: "ecommerce-b9398.firebaseapp.com",
+    projectId: "ecommerce-b9398",
+    storageBucket: "ecommerce-b9398.appspot.com",
+    messagingSenderId: "921480349731",
+    appId: "1:921480349731:web:18532d943a4133f183c996"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
