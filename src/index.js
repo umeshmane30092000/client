f536875ece95828741cce1444a8a281378cf3c9d
Umesh <umeshmane575@gmail.com>
2023-08-22 11:11:49 +0530

    imported Link from react router dom pakage so now link are clikable drive to perticular link

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import "antd/dist/antd.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
+  // <React.StrictMode>
   <BrowserRouter>
    <App />
   </BrowserRouter>
     
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
