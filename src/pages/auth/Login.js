9028ba657a575a15c88104b66238a282c74cfa5c
Umesh <umeshmane575@gmail.com>
2023-08-24 10:26:52 +0530

    Redirect Loged User that is when we loged in it will not show register page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("umesh.mane20@vit.edu");
   const [password, setPassword] = useState("123456");
   const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
 
   let dispatch = useDispatch();
 
@@ -120,6 +126,7 @@ const Login = ({ history }) => {
           >
             Login with Google
           </Button>
+
           <Link to="/forgot/password" className="float-right text-danger">
             Forgot Password
           </Link>
