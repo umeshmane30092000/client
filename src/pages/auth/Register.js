6050973a7dc878e3ebde805e37f2bb4966572f32
Umesh <umeshmane575@gmail.com>
2023-08-29 18:17:53 +0530

    all working fine

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -10,7 +10,7 @@ const Register = ({ history }) => {
 
   useEffect(() => {
     if (user && user.token) history.push("/");
-  }, [user ,history]);
+  }, [user , history]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
