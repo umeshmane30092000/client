6fbfcce68c2d26835d0ca4f77a86b4513f058b3a
Umesh <umeshmane575@gmail.com>
2023-09-14 10:46:13 +0530

    i have just modified productUpdate file and productupdateform so that we can update our service it is tough but good now we will work on update image in the next

--- src/pages/admin/product/ProductUpdate.js
+++ src/pages/admin/product/ProductUpdate.js
@@ -12,7 +12,6 @@ const initialState = {
   title: "",
   description: "",
   price: "",
-  categories: [],
   category: "",
   subs: [],
   shipping: "",
@@ -27,6 +26,10 @@ const initialState = {
 const ProductUpdate = ({ match }) => {
   // state
   const [values, setValues] = useState(initialState);
+  const [categories, setCategories] = useState([]);
+  const [subOptions, setSubOptions] = useState([]);
+  const [arrayOfSubs, setArrayOfSubs] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const { user } = useSelector((state) => ({ ...state }));
   // router
@@ -34,14 +37,33 @@ const ProductUpdate = ({ match }) => {
 
   useEffect(() => {
     loadProduct();
+    loadCategories();
   }, []);
 
   const loadProduct = () => {
     getProduct(slug).then((p) => {
       // console.log("single product", p);
+      // 1 load single proudct
       setValues({ ...values, ...p.data });
+      // 2 load single product category subs
+      getCategorySubs(p.data.category._id).then((res) => {
+        setSubOptions(res.data); // on first load, show default subs
+      });
+      // 3 prepare array of sub ids to show as default sub values in antd Select
+      let arr = [];
+      p.data.subs.map((s) => {
+        arr.push(s._id);
+      });
+      console.log("ARR", arr);
+      setArrayOfSubs((prev) => arr); // required for ant design select to work
     });
   };
+
+  const loadCategories = () =>
+    getCategories().then((c) => {
+      console.log("GET CATEGORIES IN UPDATE PRODUCT", c.data);
+      setCategories(c.data);
+    });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -53,6 +75,29 @@ const ProductUpdate = ({ match }) => {
     // console.log(e.target.name, " ----- ", e.target.value);
   };
 
+  const handleCatagoryChange = (e) => {
+    e.preventDefault();
+    console.log("CLICKED CATEGORY", e.target.value);
+    setValues({ ...values, subs: [] });
+
+    setSelectedCategory(e.target.value);
+
+    getCategorySubs(e.target.value).then((res) => {
+      console.log("SUB OPTIONS ON CATGORY CLICK", res);
+      setSubOptions(res.data);
+    });
+
+    console.log("EXISTING CATEGORY values.category", values.category);
+
+    // if user clicks back to the original category
+    // show its sub categories in default
+    if (values.category._id === e.target.value) {
+      loadProduct();
+    }
+    // clear old sub category ids
+    setArrayOfSubs([]);
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -62,13 +107,19 @@ const ProductUpdate = ({ match }) => {
 
         <div className="col-md-10">
           <h4>Product update</h4>
-          {/* {JSON.stringify(values)} */}
+          {JSON.stringify(values)}
 
           <ProductUpdateForm
             handleSubmit={handleSubmit}
             handleChange={handleChange}
             setValues={setValues}
             values={values}
+            handleCatagoryChange={handleCatagoryChange}
+            categories={categories}
+            subOptions={subOptions}
+            arrayOfSubs={arrayOfSubs}
+            setArrayOfSubs={setArrayOfSubs}
+            selectedCategory={selectedCategory}
           />
           <hr />
         </div>
