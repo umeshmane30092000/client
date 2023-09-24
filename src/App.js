561d248b2a562fd2a729d2c8e2ae5a3f5f4e0e01
Umesh <umeshmane575@gmail.com>
2023-09-24 21:09:30 +0530

    home page with categories and sub done all cliccable

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
 import ProductUpdate from "./pages/admin/product/ProductUpdate";
 import Product from "./pages/Product";
+import CategoryHome from "./pages/category/CategoryHome";
+import SubHome from "./pages/sub/SubHome";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -88,6 +90,8 @@ const App = () => {
           component={ProductUpdate}
         />
          <Route exact path="/product/:slug" component={Product} />
+         <Route exact path="/category/:slug" component={CategoryHome} />
+         <Route exact path="/sub/:slug" component={SubHome} />
       </Switch>
     </>
   );
